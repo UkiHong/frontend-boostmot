c28442d2d89ed6704175f54550f2282955eb6d47
UkiHong <suhong9507@gmail.com>
2023-04-28 19:34:38 +0100

    vvv

--- src/api.ts
+++ src/api.ts
@@ -9,7 +9,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://boostmot.onrender.com/api/v1/",
+      : "https://boostmot.onrender.com/api/v1",
   withCredentials: true,
   xsrfCookieName: "csrftoken",
   xsrfHeaderName: "X-CSRFToken",
